2bb94d82a766525b39f60904cb707a89f4b54228
Enoch <iamenochchirima@gmail.com>
2022-09-20 13:02:35 +0200

    Added the read functionality and updated the archive function

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -26,6 +26,8 @@ function compose_email() {
 
 function email_submit(event) {
 
+  event.preventDefault()
+
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
@@ -41,7 +43,7 @@ function email_submit(event) {
   });
   load_mailbox('sent');
   return false;
-}
+};
 
 
 function load_mailbox(mailbox) {
@@ -63,6 +65,7 @@ function load_mailbox(mailbox) {
       // ... do something else with emails ...
   emails.forEach(email => {
     const element = document.createElement('div');
+    element.id = "element";
     element.className = email['read'] ? "p-3 mb-2 bg-info text-white" : "p-3 mb-2 bg-white text-dark";
     element.innerHTML = `
     <span><strong>${email['sender']}</strong></span>
@@ -72,75 +75,92 @@ function load_mailbox(mailbox) {
     element.addEventListener('click', function() {
       load_email(email['id']);
       console.log('This element has been clicked!');
+
+      fetch(`/emails/${email['id']}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            read: true
+        })
+      })
+    
+      console.log('markas_read finished running')
+
     });
     document.querySelector('#emails-view').append(element);
     });
 
   });
+
+  function load_email(id) {
+
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#email-view').style.display = 'block';
+  
+    fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+        // Print email
+        console.log(email);
+  
+        // ... do something else with email ...
+        const element = document.querySelector('#email-view');
+        element.innerHTML = `
+        <div><strong>From : </strong>${email['sender']}</div>
+        <div><strong>To: </strong>${email['recipients']}</div>
+        <div><strong>Subject: </strong>${email['subject']}</div>
+        <div><strong>At: </strong>${email['timestamp']}</div>
+        <br>
+        <div>
+        <p>${email['body']}</p>
+        </div>
+        <br>
+        `;
+  
+        const reply_button = document.createElement('button');
+        reply_button.className = "btn btn-outline-primary";
+        reply_button.innerHTML = "Reply";
+        reply_button.addEventListener('click', () => {
+          compose_email();
+          const re = email.subject.slice(0, 2) === 'Re' ? '' : 'Re: ';
+          document.querySelector('#compose-recipients').value = email.sender;
+          document.querySelector('#compose-subject').value = re + email.subject;
+          document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;  
+  
+        });
+  
+        element.appendChild(reply_button);
+        
+        if (mailbox !== 'sent') {
+  
+        const archive_button = document.createElement('button');
+        archive_button.className = "btn btn-outline-primary";
+        archive_button.id = "archive-btn";
+        archive_button.innerHTML = !email['archived'] ? 'Archive' : 'Unarchive';
+        archive_button.addEventListener('click', () => {
+          if (archive_button.innerHTML === 'Archive') {
+            fetch(`/emails/${id}`, {
+              method: 'PUT',
+              body: JSON.stringify({
+                  archived: true
+              })
+            });
+          } else {
+            fetch(`/emails/${id}`, {
+              method: 'PUT',
+              body: JSON.stringify({
+                  archived: false
+              })
+            });
+          }
+  
+          load_mailbox('inbox');
+        })
+  
+        element.appendChild(archive_button);
+  
+        }
+    });
+  }  
 }
 
-function load_email(id) {
-
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'none';
-  document.querySelector('#email-view').style.display = 'block';
-
-  fetch(`/emails/${id}`)
-  .then(response => response.json())
-  .then(email => {
-      // Print email
-      console.log(email);
-
-      // ... do something else with email ...
-      const element = document.querySelector('#email-view');
-      element.innerHTML = `
-      <div><strong>From : </strong>${email['sender']}</div>
-      <div><strong>To: </strong>${email['recipients']}</div>
-      <div><strong>Subject: </strong>${email['subject']}</div>
-      <div><strong>At: </strong>${email['timestamp']}</div>
-      <br>
-      <div>
-      <p>${email['body']}</p>
-      </div>
-      <br>
-      `;
-
-      const reply_button = document.createElement('button');
-      reply_button.className = "btn btn-outline-primary";
-      reply_button.innerHTML = "Reply";
-      reply_button.addEventListener('click', () => {
-        compose_email();
-        document.querySelector('#compose-recipients').value = email.sender;
-        document.querySelector('#compose-subject').value = email.subject
-        document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote: ${email.body}`;  
-
-      });
-
-      element.appendChild(reply_button);
-
-      const archive_button = document.createElement('button');
-      archive_button.className = "btn btn-outline-primary";
-      archive_button.innerHTML = !email['archived'] ? 'Archive' : 'Unarchive';
-      archive_button.addEventListener('click', () => {
-        if (archive_button.innerHTML === 'Archive') {
-          fetch(`/emails/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                archived: true
-            })
-          });
-        } else {
-          fetch(`/emails/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify({
-                archived: false
-            })
-          });
-        }
-
-        load_mailbox('inbox');
-      })
-
-      element.appendChild(archive_button);
-  });
-}  
